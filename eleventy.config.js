42ebeca876723a55d51bbe5eb15a053c0c21f521
David W. Keith <git@dwk.io>
2025-06-18 17:00:46 -0700

    Add WebC support

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,9 @@
+import pluginWebC from "@11ty/eleventy-plugin-webc";
+
 if (!process.env.SITE) {
   console.warn("Missing SITE env, defaulting to site-1")
 }
+
 const site = process.env.SITE || "site-1";
 const input = `sites/${site}/`;
 const output = `public/${site}`;
@@ -17,6 +20,12 @@ export default function(eleventyConfig) {
   // So add that information to the global data
   eleventyConfig.addGlobalData("multisite", {input, output});
 
+  // Enable WebC (.webc) support
+  eleventyConfig.addPlugin(pluginWebC);
+
+  // Treat .webc files as template formats
+  eleventyConfig.setTemplateFormats(["html", "md", "webc"]);
+
   return {
     dir: {
       input,
@@ -24,5 +33,6 @@ export default function(eleventyConfig) {
       // Share includes so we don't need to rewrite our templates
       includes: "../../_includes",
     },
+    templateFormats: ["html", "md", "webc"],
   };
 }
